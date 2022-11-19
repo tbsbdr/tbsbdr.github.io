76f8a9eab775311a6afd0bc8914cd49299d5d125
Tobias Baader <tobias.baader@gmail.com>
2022-11-19 22:10:22 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'sans': ['Lexend', 'ui-sans-serif', 'system-ui', 'Helvetica', 'Arial', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
