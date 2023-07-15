9b372b3acc6a4b26fe8206254afc33cb60caaf62
Tobias Baader <tobias.baader@gmail.com>
2023-07-15 13:49:05 +0200

    simple black
    started new design (simple back) drafted in framer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,26 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     fontFamily: {
+<<<<<<< Updated upstream
       'sans': ['Lexend', 'ui-sans-serif', 'system-ui', 'Helvetica', 'Arial', 'sans-serif']
+=======
+      'sans': ['Chivo', 'ui-sans-serif', 'system-ui', 'Helvetica', 'Arial', 'sans-serif']
+    },
+    extend: {
+      colors:{
+        baader:{
+          dark: '#0a2540',
+          accent: '#00ccff',
+          ondark: '#adbdcc',
+          darker: '#06182c',
+          text: '#425466',
+          gradientmiddle: '#0e3358',
+          greybg: '#f6f9fc',
+          footerbg: '#0a2540',
+          onfooter: '#0a2540',
+        }
+      }
+>>>>>>> Stashed changes
     },
     extend: {},
   },
